a9f1a9c03898a1214e932f61bb008bd990de7070
TinkGu <tlwrite@126.com>
2026-02-02 16:38:37 +0800

    feat: add useSyncExternalStore and integration tests

--- src/react-atom.ts
+++ src/react-atom.ts
@@ -1,14 +1,32 @@
-import { useReducer, useLayoutEffect } from 'react';
+import { useReducer, useLayoutEffect, useEffect, useMemo } from 'react';
 import { Atom } from './atom';
 import { view } from './operators';
 
-export function useAtomView<T>(atom: Atom<T>) {
+function useSyncExternalStoreFallback<T>(subscribe: (callback: () => void) => () => void, getSnapshot: () => T): T {
   const [, forceUpdate] = useReducer((c) => c + 1, 0);
-  const state = view(atom);
+  const snapshot = getSnapshot();
 
   useLayoutEffect(() => {
-    return atom.subscribe(forceUpdate);
-  }, [atom]);
+    return subscribe(forceUpdate);
+  }, [subscribe]);
+
+  return snapshot;
+}
+
+const useSyncExternalStoreImpl: <T>(subscribe: (callback: () => void) => () => void, getSnapshot: () => T) => T =
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  typeof (useEffect as any).useSyncExternalStore === 'function'
+    ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (useEffect as any).useSyncExternalStore
+    : useSyncExternalStoreFallback;
+
+export function useAtomView<T>(atom: Atom<T>) {
+  const state = useSyncExternalStoreImpl(
+    useMemo(() => {
+      return (callback: () => void) => atom.subscribe(callback);
+    }, [atom]),
+    () => view(atom),
+  );
 
   return state;
 }
